3189e3368677dc0484c0229bd7cb5d743017ad40
Patrick-Bigodss <Patrick942009@hotmail.com>
2026-07-14 21:44:22 -0300

    atividade 4 (não finalizada)

---
+++ src/atividade4/exercicio1.java
@@ -0,0 +1,40 @@
+package atividade4;
+
+import java.util.Scanner;
+
+public class exercicio1 {
+
+    public static void abrirCofre() {
+        Scanner entrada = new Scanner(System.in);
+
+        int codigo ;
+        int tentativas = 5;
+
+        while (tentativas > 0) {
+
+            System.out.print("Digite o código do cofre: ");
+            codigo = entrada.nextInt();
+
+            if (codigo == 9999) {
+                System.out.println("Cofre Aberto!");
+                break;
+            } else {
+                System.out.println("Código errado!");
+                tentativas--;
+            }
+
+            if (tentativas > 0) {
+                System.out.println("Código Errado. Restam " + tentativas + " tentativa(s).");
+            } else {
+                System.out.println("Cofre Bloqueado!");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        abrirCofre();
+
+    }
+    }
+
