9723c6f06f6fa3c8495c1c9bf322cd7604f3d1a8
Patrick-Bigodss <Patrick942009@hotmail.com>
2026-07-21 20:30:12 -0300

    finalizando o encapsulamento

--- src/aulasJAVA/encapsulamento/modelo/ContaBancaria.java
+++ src/aulasJAVA/encapsulamento/modelo/ContaBancaria.java
@@ -1,37 +1,50 @@
 package aulasJAVA.encapsulamento.modelo;
 
 public class ContaBancaria {
+
     //Atributos dessa classe
     private String titular;
     private int numeroDaConta;
     private double saldo;
-    private static int NovoNumeroDaConta  = 1000;
+    private static int NovoNumeroDaConta  = 100020;
 
-    //Construtor dessa classe
+    @Override
+    public String toString() {
+        return "ContaBancaria{" +
+                "titular='" + titular + '\''+
+                ",numeroDaConta="+ numeroDaConta +
+                ", saldo=" + saldo +
+                '}';
+    }
+
+    //metodo Construtor dessa classe
     public ContaBancaria(String titular) {
         this.titular = titular;
         this.numeroDaConta = NovoNumeroDaConta;
         NovoNumeroDaConta++;
-        this.saldo =0.0;
+        this.saldo= 0;
     }
+
 
     // é um metodo para pegar o valor dentro da instância da classe (no objeto)
     public String getTitular() {
         return titular;
     }
 
-    public int getNumeroDaConta() {
+    public int getNumeroDaConta () {
         return numeroDaConta;
     }
 
     // é um metodo para modificar o valor dentro da instância da classe (no objeto) - setTitular
+    //public String setTitular() {
+        //return titular;
 
     // um metodo criado para modificar o valor do saldo de um objeto
 
     public void depositar(double valor){
         if (valor > 0){
             this.saldo += valor;
-            System.out.println(valor+ "foi depositado");
+            System.out.println(valor+ ".Confirmado");
         } else {
             System.out.println("Erro: Valor Inválido");
         }
--- src/aulasJAVA/encapsulamento/principal/Main.java
+++ src/aulasJAVA/encapsulamento/principal/Main.java
@@ -2,14 +2,38 @@ package aulasJAVA.encapsulamento.principal;
 
 import aulasJAVA.encapsulamento.modelo.ContaBancaria;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        ContaBancaria MinhaConta = new ContaBancaria("Patrick Leal");
-        System.out.println(MinhaConta.getTitular());
-    }
-    ContaBancaria conta1 = new ContaBancaria("Patrick");
-    ContaBancaria conta2 = new ContaBancaria("João");
-    ContaBancaria conta3 = new ContaBancaria("Rosangela");
+        ContaBancaria conta1 = new ContaBancaria("Patrick Leal");
+        ContaBancaria conta2 = new ContaBancaria("Rosangela");
+        ContaBancaria conta3 = new ContaBancaria("João");
 
+        //conta1.depositar(500);
+        //conta2.depositar(300);
+        //conta3.depositar(2000);
+        //conta2.sacar(100);
+
+        System.out.println("a pessoa:"+ conta1.getTitular()+ "depositou:");
+        conta1.depositar(500);
+
+        System.out.println("a pessoa:"+ conta2.getTitular()+ "depositou");
+        conta2.depositar(300);
+
+        System.out.println("a pessoa:"+ conta3.getTitular()+ "depositou");
+        conta3.depositar(2000);
+
+        System.out.println("a pessoa:"+ conta2.getTitular()+ "sacou");
+        conta2.sacar(100);
+
+
+
+        //System.out.println(conta1);
+        //System.out.println(conta2);
+        //System.out.println(conta3);
+    }
 
 }
+
+
