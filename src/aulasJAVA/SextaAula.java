eadab4689f91d27c9331afc36f56402306b82ebc
Patrick-Bigodss <Patrick942009@hotmail.com>
2026-07-20 21:24:51 -0300

    ajeitando o codigo da sexta aula

--- src/aulasJAVA/SextaAula.java
+++ src/aulasJAVA/SextaAula.java
@@ -4,23 +4,21 @@ import aulasJAVA.objeto.Pessoa;
 
 public class SextaAula {
     public static void main(String[] args) {
-        Pessoa pessoa1 = new Pessoa();
-        pessoa1.nome ="Patrick";
-        pessoa1.anoNascimento = 1994;
-        pessoa1.genero = 'M';
-        pessoa1.cor = "Pardo";
+        Pessoa pessoa1 = new Pessoa(
+                "João",
+                2004,
+                'M',
+                "Branco",
+                "Professor"
+        );
 
-
-        Pessoa pessoa2 = new Pessoa();
-        pessoa2.nome ="Grace";
-        pessoa2.anoNascimento = 1991;
-        pessoa2.genero = 'F';
-        pessoa2.cor = "Branca";
+        System.out.println(pessoa1.getNome());
+        ;
 
         System.out.println(pessoa1);
-        System.out.println(pessoa2);
 
+        pessoa1.setNome("Fulano de tal");
 
+        System.out.println(pessoa1);
     }
-
 }
